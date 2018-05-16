b31dd775d1190139d3cc9f8e9bec843d6135cfe9
John J Czaplewski <john@czaplewski.org>
2018-05-16 12:51:39 -0500

    Rework for async/await

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ class pgHelper {
       process.exit(-1)
     })
   }
-  
-  query(sql, params, callback) {
-    this.pool.connect((error, client, done) => {
-      if (error) return callback(error)
-      client.query(sql, params, (err, res) => {
-        done()
 
-        if (err) {
-          return callback(err.stack)
-        }
-        callback(null, res.rows)
-      })
-    })
+  // Standard query
+  async query(sql, params) {
+    const client = await this.pool.connect()
+    try {
+      const res = await client.query(sql, params)
+      return res.rows
+    } catch(e) {
+      throw e
+    } finally {
+      client.release()
+    }
   }
-  
+
+  // Return a client. Useful for things like streams. NEED TO RELEASE CLIENT MANUALLY
+  async getClient() {
+    const client = await this.pool.connect()
+    return client
+  }
+
+  // Stream a query. Useful for piping to dbgeo or res
   queryStream(sql, params, raw=false) {
     // a readable stream to return
     let stream
@@ -34,7 +40,7 @@ class pgHelper {
     } else {
       stream = JSONStream.stringify('[', ',', ']')
     }
-    
+
     this.pool.connect((err, client, done) => {
       if (err) throw err
       let query = new queryStream(sql, params)
@@ -43,11 +49,11 @@ class pgHelper {
       pgStream.on('end', () => {
         done()
       })
-      
+
       pgStream.pipe(stream)
     })
     return stream
   }
-} 
+}
 
 module.exports = pgHelper
